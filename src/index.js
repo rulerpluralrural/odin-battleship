ce0c5b9ce9f0bb6608455e9995c704d9f54fa5c8
Gerald Javier <33073866+rulerpluralrural@users.noreply.github.com>
2023-09-02 20:04:44 +0800

    Created board.js this creates player boards

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-import Gameboard from "./Main/Classes/Gameboard.js";
+import { createBoards } from "./Game/Board.js";
 import { startMenu } from "./Game/Modal.js";
+import BoardController from "./Main/Classes/BoardController.js";
+import Game from "./Main/Classes/GameController.js";
 import "./index.css";
 
-// const gameboard = new Gameboard()
-// console.table(gameboard.board)
-// gameboard
+
 const root = document.getElementById("root");
+
 window.onload = () => {
 	startMenu()
+	root.appendChild(createBoards())
+
+	const playerBoard = document.querySelector('#player-board')
+	const enemyBoard = document.querySelector('#enemy-board')
+
+	const player1 = Game.player
+	const player2 = Game.AI
+
+	Game.startGame(
+		new BoardController(player1, playerBoard),
+			new BoardController(player2, enemyBoard)
+	)
+	
+	console.log(playerBoard)
+	console.log(enemyBoard)
 };
