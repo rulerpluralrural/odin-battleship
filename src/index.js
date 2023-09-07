44a6b573fe883f21e1834c9170dcff0f6443c3de
Gerald Javier <33073866+rulerpluralrural@users.noreply.github.com>
2023-09-07 21:04:36 +0800

    Added smartRandom Attack for AI

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import { createBoards } from "./Game/Board.js";
 import { startMenu } from "./Game/StartMenu.js";
-import {overlay} from "./Game/EndGameModal.js";
+import { overlay } from "./Game/EndGameModal.js";
 import "./index.css";
-
 
 const root = document.getElementById("root");
 
 window.onload = () => {
-	startMenu()
-	root.appendChild(createBoards())
-	root.appendChild(overlay)
-}
+	startMenu();
+	root.appendChild(createBoards());
+	root.appendChild(overlay);
+};
